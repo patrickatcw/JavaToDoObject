a7533abe8452fb57c111795f8d234fca9b237bfc
patrickatcw <pj8392@yahoo.com>
2018-02-17 10:36:12 -0500

    fields, constructors, and getter methods complete, work in Main on next on instances for objects and printout logic

--- src/com/me/ToDo.java
+++ src/com/me/ToDo.java
@@ -1,4 +1,37 @@
 package com.me;
 
 public class ToDo {
+
+    //fields
+    public int wake;
+    public String next;
+    public String work;
+    public String lunch;
+
+    //constructors w/parameters
+    public ToDo(int wake, String next, String work, String lunch) {
+
+        this.wake = wake;
+        this.next = next;
+        this.work = work;
+        this.lunch = lunch;
+
+    }
+
+    //getter methods for this. fields
+    public int getWake() {
+        return this.wake;
+    }
+
+    public String getNext(){
+        return this.next;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setLunch(String lunch) {
+        this.lunch = lunch;
+    }
 }
